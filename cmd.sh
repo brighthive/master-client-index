0327e92129a16e141709aceb89af3d1d2076b22f
Gregory Mundy <gregmundy@gmail.com>
2019-04-01 14:28:10 -0400

    chore: add better logic to launch script to handle the needs of various environments

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0 mci:app
+MAX_RETRIES=5
+MIGRATION_PATH=mci/db/migrations
+
+if [ "$APP_ENV" == "SANDBOX" ]; then
+    RETRIES=0
+    until flask db upgrade -d $MIGRATION_PATH; do
+        RETRIES=`expr $RETRIES + 1`
+        if [[ "$RETRIES" -eq "$MAX_RETRIES" ]]; then
+            echo "Retry Limit Exceeded. Aborting..."
+            exit 1
+        fi
+        sleep 2
+    done
+fi
+
+if [ "$APP_ENV" == "DEVELOPMENT" ] || [ -z "$APP_ENV" ]; then
+    gunicorn -b 0.0.0.0 mci:app --reload
+else
+    gunicorn -b 0.0.0.0 mci:app
+fi
